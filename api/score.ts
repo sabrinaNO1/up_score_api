7ccdea4e3b6a6a37d1c935ac4afbe766d38428f5
sabrinaNO1 <1425139066@qq.com>
2025-10-08 16:41:43 +0800

    Update score.ts

--- api/score.ts
+++ api/score.ts
@@ -1,5 +1,4 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import fetch from 'node-fetch';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const email = req.query.email as string;
